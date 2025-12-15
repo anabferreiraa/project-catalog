38f7e6a32b476153e3fd10d45f02879d9934fdbd
Ana Beatriz <anabferreira0203@gmail.com>
2025-12-15 19:30:51 -0300

    instalação de projeto com typescipit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import AnnouncementBar from "$lib/Components/AnnouncementBar/AnnouncementBar.svelte";
+
+</script>
+<AnnouncementBar messages = {['frete gratis pra todo o Brasil' , '10% off no pix']} />
