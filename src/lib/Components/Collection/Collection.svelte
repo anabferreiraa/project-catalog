5148fcde2002c9421bc0e2f6f2c4f75494940b2a
Ana Beatriz <anabferreira0203@gmail.com>
2026-02-20 20:07:37 -0300

    carrossel part2

--- src/lib/Components/Collection/Collection.svelte
+++ src/lib/Components/Collection/Collection.svelte
@@ -1,23 +1,55 @@
 <script>
 	import ProductCard from '../ProductCard/ProductCard.svelte';
-
 	const { titleCollection = 'coleção', products = [{}] } = $props();
+	import { onMount } from 'svelte';
+	const ir = '>'
+	const voltar = '<'
+     let carousel; // referência da div
+	let scrollAmount = 0;
+	onMount(() => {
+		// largura de um card + gap
+		const card = carousel?.querySelector('div');
+		scrollAmount = card?.offsetWidth + 24; // 24 = gap-6 (6*4px)
+	});
+
+	function next() {
+		carousel.scrollBy({
+			left: scrollAmount,
+			behavior: 'smooth'
+		});
+	}
+
+	function prev() {
+		carousel.scrollBy({
+			left: -scrollAmount,
+			behavior: 'smooth'
+		});
+	}
 </script>
 
 <section class=" bg-gray-50/30 py-10">
-	<div class="container mx-auto flex flex-col gap-6" >
+	<div class="flex flex-col gap-6 pl-6" >
 		<div>
 			<h2 class="text-2xl font-bold">
 				{titleCollection}
 			</h2>
 		</div>
-		<div class="flex pb-3 snap-x snap-mandatory gap-6 overflow-x-auto scroll-smooth ">
+		
+           <div class="flex pb-3 snap-x snap-mandatory gap-6 overflow-x-auto no-scrollbar scroll-smooth " bind:this={carousel}>
 			{#each products as { sku, images, name, description, variation, price }}
-				<div class="shrink-0 snap-center snap-always">
+				<div class="shrink-0 snap-center snap-normal">
 					<ProductCard {sku} {images} {name} {description} {variation} {price} />
 				</div>
 			{/each}
 		</div>
-		<hr>
-	</div>
+		<div class=" flex gap-3 items-center px-10 ">
+			<div class="w-full h-px rounded-full bg-gray-600"></div>
+			<div class="lg:flex gap-3 hidden ">
+				<button class=" w-9 h-9 rounded-full bg-white shadow-md cursor-pointer "on:click={prev} > {voltar} </button>
+				<button class=" w-9 h-9 rounded-full bg-white shadow-md cursor-pointer " on:click={next}> {ir}</button>
+			</div>
+		</div>
+		</div>
+ 
+
 </section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,9 +3,8 @@
 	import BannerCarousel from '$lib/Section/Banner/BannerCarousel.svelte';
 	import Collection from '$lib/Components/Collection/Collection.svelte';
 	import exemplo from '$cms/exemplo.json'
+
 </script>
 
 <BannerCarousel banners={bannerDate.banners} />
-
-<Collection   titleCollection = {exemplo.nameCollection} products = {exemplo.products}  />
-
+<Collection titleCollection = {exemplo.nameCollection} products = {exemplo.products}  />
